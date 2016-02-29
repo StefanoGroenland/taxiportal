72e710b23a3b52bc52a7cedb53da4574657d021b
Stefano Groenland <stefano.groenland@gmail.com>
2016-02-29 11:16:23 +0100

    API funct tablet login vergat een json return -,-"

--- app/Http/Controllers/ApiOneController.php
+++ app/Http/Controllers/ApiOneController.php
@@ -210,7 +210,7 @@ class ApiOneController extends Controller
             if($exists > 0){
                 $taxi = Taxi::with('driver')->where('id','=',$tablet->tablet->taxi_id)->first();
                 $result = collect([$tablet,$taxi,$taxi->driver->user]);
-                return $result;
+                return json_encode($result);
             }
             return json_encode(array('error' => 'no_tablet_found', 'api-version' => '1.0'));
         }
