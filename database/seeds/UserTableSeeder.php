551d6f76c8d14c82934575712d546b770ac49498
Stefano Groenland <stefano.groenland@gmail.com>
2016-03-04 16:26:18 +0100

    merge fix?

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('user')->insert([
+            'email'         => 'stefano@moodles.nl',
+            'password'      => bcrypt('moodles'),
+            'firstname'     => 'Stefano',
+            'surname'       => '',
+            'lastname'      => 'Groenland',
+            'profile_photo' => str_random(5),
+            'phone_number'  => str_random(5),
+            'user_rank'     => 'admin',
+        ]);
+    }
+}
