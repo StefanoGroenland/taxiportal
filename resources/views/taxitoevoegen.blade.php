7545c93b682db4d81aabe8576a450daf6b5bdfd7
Richard Perdaan <richard@moodles.nl>
2016-03-17 15:31:57 +0100

    formulier taxi toevoegen verbeterd

--- resources/views/taxitoevoegen.blade.php
+++ resources/views/taxitoevoegen.blade.php
@@ -82,6 +82,7 @@
                                            			</div>
                                 				</div>
                                 				<div class="col-lg-2 col-md-6 col-sm-6 col-xs-12">
+
                                                     <label for="geslacht">Chauffeur optie</label>
                                                     <div class="form-group" data-validate="group">
                                                         <label class="radio-inline">
@@ -93,110 +94,129 @@
                                                     </div>
                                                 </div>
                                            </div>
-                                           <div class="row">
-                                            <div class="col-lg-12">
-                                                <fieldset id="fieldset-driver" class="hide" disabled>
-                                                <div class="row margin-top-40">
-                                                <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12 center-block text-center">
-                                                    <div class="fileinput fileinput-new " data-provides="fileinput">
-                                                        <div id="jcrop_target" class="fileinput-new thumbnail center-block" style="width: 200px; height: 200px;">
-                                                            <img id="jcrop_target" style=" height:100%; width:100%;" src="../assets/img/avatars/avatar.png" alt="avatar" class="img-responsive center-block"/>
-                                                            <div class="jcrop-holder" style="width: 400px !important; height: 200px!important;"></div>
-                                                        </div>
-                                                        <div>
-                                                            <span class="btn btn-success" id="verkennerButton" data-toggle="tooltip" title="Kies een foto" onclick="$(this).parent().find('input[type=file]').click();">Verkenner</span>
-                                                            <input name="profile_photo" id="imgInp" onchange="$(this).parent().parent().find('.form-control').html($(this).val().split(/[\\|/]/).pop());readURL(this)" style="display: none;" type="file">
-                                                        </div>
-                                                    </div>
-                                                    </div>
-                                                	    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                                                           <div class="form-group form-md-line-input">
-                                                               <div class="input-icon">
-                                                                   <input type="text" class="form-control" id="email" name="email" data-validate="required|email|max:50" value="{{old('email')}}">
-                                                                   <label for="email">E-mail</label>
-                                                                   <i class="fa fa-envelope-o"></i>
-                                                               </div>
-                                                           </div>
-                                                       </div>
-                                                       <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                                                           <div class="form-group form-md-line-input">
-                                                               <div class="input-icon">
-                                                                    <input type="text" class="form-control" id="telefoonnummer" name="phonenumber" data-validate="required|number|minlength:10|maxlength:10" value="{{old('phone_number')}}">
-                                                                   <label for="telefoonnummer">Telefoonnummer</label>
-                                                                   <i class="fa fa-phone"></i>
-                                                               </div>
-                                                           </div>
-                                                       </div>
-                                                       <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12">
-                                                           <div class="form-group form-md-line-input">
-                                                               <div class="input-icon">
-                                                                   <input type="text" class="form-control" id="voornaam" name="firstname" data-validate="required" value="{{old('firstname')}}">
-                                                                   <label for="voornaam">Voornaam</label>
-                                                                   <i class="fa fa-user"></i>
-                                                               </div>
-                                                           </div>
-                                                       </div>
-                                                       <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12">
-                                                           <div class="form-group form-md-line-input">
-                                                               <div class="input-icon">
-                                                                   <input type="text" class="form-control" id="achternaam" name="lastname" data-validate="required" value="{{old('lastname')}}">
-                                                                   <label for="achternaam">Achternaam</label>
-                                                                   <i class="fa fa-user"></i>
-                                                               </div>
-                                                           </div>
-                                                       </div>
-                                                       <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12 margin-top-40">
-                                                           <div class="form-group form-md-line-input">
-                                                               <div class="input-icon">
-                                                                   <input type="password" class="form-control" id="password" name="password" data-validate="required|minlength:4|same:#password_confirmation" data-name="Wachtwoord">
-                                                                   <label for="wachtwoord">Wachtwoord</label>
-                                                                   <i class="fa fa-key"></i>
-                                                               </div>
-                                                           </div>
-                                                       </div>
-                                                       <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12 margin-top-40">
-                                                           <div class="form-group form-md-line-input">
-                                                               <div class="input-icon">
-                                                                   <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" data-validate="required|minlength:4|same:#password" data-name="Herhaal wachtwoord">
-                                                                   <label for="herhaal_wachtwoord">Herhaal wachtwoord</label>
-                                                                   <i class="fa fa-key"></i>
-                                                               </div>
-                                                           </div>
-                                                       </div>
-                                                       <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12 margin-top-40">
-                                                           <label for="geslacht">Geslacht</label>
-                                                           <div class="form-group" data-validate="group">
-                                                               <label class="radio-inline" >
-                                                                   <input type="radio" name="sex" id="radman" class="md-radiobtn" value="man" checked="" @if(old('sex') == 'man') checked @endif> Man
-                                                               </label>
-                                                               <label class="radio-inline">
-                                                                   <input type="radio" name="sex" id="radvrouw" class="md-radiobtn" value="vrouw" @if(old('sex') == 'vrouw') checked @endif> Vrouw
-                                                               </label>
-                                                           </div>
-                                                       </div>
-                                                       <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12 margin-top-40">
-                                                           <div class="form-group form-md-line-input ">
-                                                               <div class="input-icon">
-                                                                   <input type="number" class="form-control" id="driver_exp" name="driver_exp" data-validate="required|number" value="{{old('drivers_exp')}}">
-                                                                   <label for="driver_exp">Rijervaring (jaren)</label>
-                                                                   <i class="fa fa-user"></i>
-                                                               </div>
-                                                           </div>
-                                                       </div>
-                                                        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 margin-top-40">
-                                                           <div class="form-group form-md-line-input ">
-                                                               <div class="input-icon">
-                                                                   <textarea class="form-control" id="global_information" name="global_information" rows="3">{{old('global_information')}}</textarea>
-                                                                   <label for="global_information">Informatie</label>
-                                                                   <i class="fa fa-info"></i>
-                                                               </div>
-                                                           </div>
-                                                       </div>
-                                                </div>
-                                                </fieldset>
-                                            </div>
-                                           </div>
-                                           </div>
+										<div class="row">
+											<div class="col-lg-12">
+												<fieldset id="fieldset-driver" class="hide" disabled>
+													<hr>
+													<i class="fa fa-plus font-grey-gallery"></i>
+													<span class="caption-subject bold uppercase"> Chauffeur toevoegen</span>
+													<div class="row margin-top-40">
+														<input type="hidden" id="x" name="x">
+														<input type="hidden" id="y" name="y">
+														<input type="hidden" id="w" name="w">
+														<input type="hidden" id="h" name="h">
+														<div class="profile-sidebar">
+															<div class="profile-sidebar-portlet">
+																<div class="profile-usertitle">
+																	<div class="row">
+																		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 center-block">
+																			<div class="fileinput fileinput-new " data-provides="fileinput">
+																				<div id="jcrop_target" class="fileinput-new thumbnail center-block" style="width: 200px; height: 200px;">
+																					<img id="jcrop_target" style=" height:100%; width:100%;" src="../assets/img/avatars/avatar.png" alt="avatar" class="img-responsive center-block"/>
+																					<div class="jcrop-holder" style="width: 400px !important; height: 200px!important;"></div>
+																				</div>
+																				<div>
+																					<span class="btn btn-success" id="verkennerButton" data-toggle="tooltip" title="Kies een foto" onclick="$(this).parent().find('input[type=file]').click();">Verkenner</span>
+																					<input name="profile_photo" id="imgInp" onchange="$(this).parent().parent().find('.form-control').html($(this).val().split(/[\\|/]/).pop());readURL(this)" style="display: none;" type="file">
+																				</div>
+																			</div>
+																		</div>
+																	</div>
+																</div>
+															</div>
+														</div>
+														<div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+															<div class="row">
+																<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+																	<div class="form-group form-md-line-input">
+																		<div class="input-icon">
+																			<input type="text" class="form-control" id="email" name="email" data-validate="required|email|max:50" value="{{old('email')}}">
+																			<label for="email">E-mail</label>
+																			<i class="fa fa-envelope-o"></i>
+																		</div>
+																	</div>
+																</div>
+																<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+																	<div class="form-group form-md-line-input">
+																		<div class="input-icon">
+																			<input type="text" class="form-control" id="telefoonnummer" name="phonenumber" data-validate="required|number|minlength:10|maxlength:10" value="{{old('phone_number')}}">
+																			<label for="telefoonnummer">Telefoonnummer</label>
+																			<i class="fa fa-phone"></i>
+																		</div>
+																	</div>
+																</div>
+																<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+																	<div class="form-group form-md-line-input">
+																		<div class="input-icon">
+																			<input type="text" class="form-control" id="voornaam" name="firstname" data-validate="required" value="{{old('firstname')}}">
+																			<label for="voornaam">Voornaam</label>
+																			<i class="fa fa-user"></i>
+																		</div>
+																	</div>
+																</div>
+																<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+																	<div class="form-group form-md-line-input">
+																		<div class="input-icon">
+																			<input type="text" class="form-control" id="achternaam" name="lastname" data-validate="required" value="{{old('lastname')}}">
+																			<label for="achternaam">Achternaam</label>
+																			<i class="fa fa-user"></i>
+																		</div>
+																	</div>
+																</div>
+																<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+																	<div class="form-group form-md-line-input">
+																		<div class="input-icon">
+																			<input type="password" class="form-control" id="password" name="password" data-validate="required|minlength:4|same:#password_confirmation" data-name="Wachtwoord">
+																			<label for="wachtwoord">Wachtwoord</label>
+																			<i class="fa fa-key"></i>
+																		</div>
+																	</div>
+																</div>
+																<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+																	<div class="form-group form-md-line-input">
+																		<div class="input-icon">
+																			<input type="password" class="form-control" id="password_confirmation" name="password_confirmation" data-validate="required|minlength:4|same:#password" data-name="Herhaal wachtwoord">
+																			<label for="herhaal_wachtwoord">Herhaal wachtwoord</label>
+																			<i class="fa fa-key"></i>
+																		</div>
+																	</div>
+																</div>
+																<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+																	<label for="geslacht">Geslacht</label>
+																	<div class="form-group" data-validate="group">
+																		<label class="radio-inline" >
+																			<input type="radio" name="sex" id="radman" class="md-radiobtn" value="man" checked="" @if(old('sex') == 'man') checked @endif> Man
+																		</label>
+																		<label class="radio-inline">
+																			<input type="radio" name="sex" id="radvrouw" class="md-radiobtn" value="vrouw" @if(old('sex') == 'vrouw') checked @endif> Vrouw
+																		</label>
+																	</div>
+																</div>
+																<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+																	<div class="form-group form-md-line-input">
+																		<div class="input-icon">
+																			<input type="number" class="form-control" id="driver_exp" name="driver_exp" data-validate="required|number" value="{{old('drivers_exp')}}">
+																			<label for="driver_exp">Rijervaring (jaren)</label>
+																			<i class="fa fa-user"></i>
+																		</div>
+																	</div>
+																</div>
+																<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+																	<div class="form-group form-md-line-input">
+																		<div class="input-icon">
+																			<textarea class="form-control" id="global_information" name="global_information" rows="3">{{old('global_information')}}</textarea>
+																			<label for="global_information">Informatie</label>
+																			<i class="fa fa-info"></i>
+																		</div>
+																	</div>
+																</div>
+															</div>
+														</div>
+													</div>
+												</fieldset>
+											</div>
+										</div>
+										</div>
                                         <div class="row">
                                             <div class="col-lg-12">
                                                 <div class="form-actions noborder pull-right ">
