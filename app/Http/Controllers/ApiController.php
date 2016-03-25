9f169a947f3d8c3e3403a692c955e8fea96e6f77
Stefano Groenland <stefano.groenland@gmail.com>
2016-03-25 10:58:18 +0100

    API func om ads op te halen dmv locatie en type banner is nu ook uitgewerkt.

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -101,18 +101,27 @@ class ApiController extends Controller
     public function adsByType()
     {
         $type       = Input::get('type');
+        $location   = Input::get('location');
         $key        = Input::get('key');
-        if(!empty($type)){
+
+        if(!empty($type) && !empty($location)){
             if ($key == self::$apikey) {
-                $results = Ad::with('adLocation')->where('type', '=', $type)->get();
+                $results = AdLocation::with('ad')->where('location', '=', $location)->get();
+
+                $response[] = array();
+                foreach($results as $key => $value){
+                    if($value->ad->type == $type){
+                        $response[] = $value;
+                    }
+                }
 
                 if($results->isEmpty()){
                     return response()->json(self::$none, 404);
                 }else{
                     return response()->json(array(
-                        'advertisements'    =>  $results,
+                        'advertisements'    =>  $response,
                         'success'           =>  true,
-                        'action'            =>  'get_ads_for_location',
+                        'action'            =>  'get_ads_for_location_and_type',
                         'status'            =>  '200'
                     ),200);
                 }
